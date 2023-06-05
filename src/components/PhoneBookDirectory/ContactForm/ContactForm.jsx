94a2ef6fdcd8c44cc31a3bc44941f9a8036111ce
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-06-05 14:06:16 +0300

    update paterns

--- src/components/PhoneBookDirectory/ContactForm/ContactForm.jsx
+++ src/components/PhoneBookDirectory/ContactForm/ContactForm.jsx
@@ -3,7 +3,7 @@ import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 import { Container, Button, Input, Label, Form } from './ContactForm.styled';
 
-const ContactForm = ({ contacts, onAddContact }) => {
+const ContactForm = ({ onAddContact }) => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
@@ -38,7 +38,7 @@ const ContactForm = ({ contacts, onAddContact }) => {
         <Input
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           value={name}
@@ -48,7 +48,7 @@ const ContactForm = ({ contacts, onAddContact }) => {
         <Input
           type="tel"
           name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           value={number}
--- src/components/PhoneBookDirectory/PhoneBookApp.jsx
+++ src/components/PhoneBookDirectory/PhoneBookApp.jsx
@@ -4,7 +4,7 @@ import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import Footer from './Footer/Footer';
-import { Wraper } from './App.styled';
+import { Wraper as Wrapper } from './App.styled';
 
 const PhoneBook = () => {
   const [contacts, setContacts] = useState([]);
@@ -50,7 +50,7 @@ const PhoneBook = () => {
 
   return (
     <>
-      <Wraper>
+      <Wrapper>
         <h1>Phonebook</h1>
         <ContactForm contacts={contacts} onAddContact={handleAddContact} />
 
@@ -60,7 +60,7 @@ const PhoneBook = () => {
           contacts={filteredContacts}
           onDeleteContact={handleDeleteContact}
         />
-      </Wraper>
+      </Wrapper>
       <Footer />
     </>
   );
@@ -77,6 +77,88 @@ PhoneBook.propTypes = {
 };
 
 export default PhoneBook;
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import PropTypes from 'prop-types';
+// import ContactForm from './ContactForm/ContactForm';
+// import ContactList from './ContactList/ContactList';
+// import Filter from './Filter/Filter';
+// import Footer from './Footer/Footer';
+// import { Wraper } from './App.styled';
+
+// const PhoneBook = () => {
+//   const [contacts, setContacts] = useState([]);
+//   const [filter, setFilter] = useState('');
+
+//   useEffect(() => {
+//     const savedContacts = localStorage.getItem('contacts');
+//     if (savedContacts) {
+//       setContacts(JSON.parse(savedContacts));
+//     }
+//   }, []);
+
+//   useEffect(() => {
+//     localStorage.setItem('contacts', JSON.stringify(contacts));
+//   }, [contacts]);
+
+//   const handleFilterChange = event => {
+//     setFilter(event.target.value);
+//   };
+
+//   const handleAddContact = newContact => {
+//     const isExistingContact = contacts.some(
+//       contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+//     );
+
+//     if (isExistingContact) {
+//       alert(`${newContact.name} is already in contacts`);
+//       return;
+//     }
+
+//     setContacts(prevContacts => [...prevContacts, newContact]);
+//   };
+
+//   const handleDeleteContact = contactId => {
+//     setContacts(prevContacts =>
+//       prevContacts.filter(contact => contact.id !== contactId)
+//     );
+//   };
+
+//   const filteredContacts = contacts.filter(contact =>
+//     contact.name.toLowerCase().includes(filter.toLowerCase())
+//   );
+
+//   return (
+//     <>
+//       <Wraper>
+//         <h1>Phonebook</h1>
+//         <ContactForm contacts={contacts} onAddContact={handleAddContact} />
+
+//         <h2>Contacts</h2>
+//         <Filter value={filter} onChange={handleFilterChange} />
+//         <ContactList
+//           contacts={filteredContacts}
+//           onDeleteContact={handleDeleteContact}
+//         />
+//       </Wraper>
+//       <Footer />
+//     </>
+//   );
+// };
+
+// PhoneBook.propTypes = {
+//   contacts: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.string.isRequired,
+//       name: PropTypes.string.isRequired,
+//       number: PropTypes.string.isRequired,
+//     })
+//   ),
+// };
+
+// export default PhoneBook;
 
 
 // import { Component } from 'react';
