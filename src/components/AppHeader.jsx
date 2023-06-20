dacb27413f99473666eeba1ebd224a4d15b4d684
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-06-20 16:22:39 +0300

    Update AppHeader.jsx

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,81 +1,81 @@
 import React from 'react';
 import { FaArrowLeft } from 'react-icons/fa';
+import styled, { keyframes } from 'styled-components';
 
-const AppHeader = ({ setCurrentPage, currentPage  }) => {
+const AppHeader = ({ setCurrentPage, currentPage }) => {
   const handleGoBack = () => {
     setCurrentPage(null);
   };
 
   if (currentPage === null) {
-    return null; 
+    return null;
   }
 
-  return (
-    <div
-     style={{
-    position: 'absolute',
-    zIndex: '1300',
-    left: 0,
-    top: 0,
-    display: 'flex',
-    alignItems: 'center',
-    padding: '0 10px',
-    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-    borderRadius: '4px',
-    transition: '0.3s',
-    overflow: 'hidden',
-    ":hover": {
-        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.4)',
-        transform: 'scale(1.1)',
-    },
-    "::before": {
-        content: '""',
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        border: '2px solid transparent',
-        borderRadius: '4px',
-        animation: 'border-animation 1s infinite',
-        zIndex: -1,
-        pointerEvents: 'none',
-    },
-    ":hover::before": {
-        animation: 'none',
-        borderColor: '#ff0000',
-    },
-    "@keyframes border-animation": {
-        "0%": {
-            borderColor: 'transparent',
-        },
-        "50%": {
-            borderColor: '#ff0000',
-        },
-        "100%": {
-            borderColor: 'transparent',
-        },
-    },
-}}
+  const borderAnimation = keyframes`
+    0% {
+      border-color: transparent;
+    }
+    50% {
+      border-color: #ff0000;
+    }
+    100% {
+      border-color: transparent;
+    }
+  `;
 
-    onClick={handleGoBack}
-    >
-      <button
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          background: 'none',
-          border: 'none',
-          outline: 'none',
-          cursor: 'pointer',
-          fontSize: '18px',
-          fontWeight: 'bold',
-        }}
-      >
+  const Container = styled.div`
+    position: absolute;
+    z-index: 1300;
+    left: 0;
+    top: 0;
+    display: flex;
+    align-items: center;
+    padding: 0 10px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    border-radius: 4px;
+    transition: 0.3s;
+    overflow: hidden;
+    &:hover {
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
+      transform: scale(1.1);
+    }
+    &::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      border: 2px solid transparent;
+      border-radius: 4px;
+      animation: ${borderAnimation} 1s infinite;
+      z-index: -1;
+      pointer-events: none;
+    }
+    &:hover::before {
+      animation: none;
+      border-color: #ff0000;
+    }
+  `;
+
+  const StyledButton = styled.button`
+    display: flex;
+    align-items: center;
+    background: none;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    font-size: 18px;
+    font-weight: bold;
+  `;
+
+  return (
+    <Container onClick={handleGoBack}>
+      <StyledButton>
         <FaArrowLeft style={{ marginRight: '5px' }} />
-        Go Back 
-      </button>
-    </div>
+        Go Back
+      </StyledButton>
+    </Container>
   );
 };
 
