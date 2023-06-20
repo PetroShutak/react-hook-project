a71b91e42f98086a569ee88823bf0542ac0289df
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-06-20 16:17:32 +0300

    Update AppHeader.jsx
    styled button "Go back"

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -12,19 +12,52 @@ const AppHeader = ({ setCurrentPage, currentPage  }) => {
 
   return (
     <div
-      style={{
+     style={{
+    position: 'absolute',
+    zIndex: '1300',
+    left: 0,
+    top: 0,
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0 10px',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+    borderRadius: '4px',
+    transition: '0.3s',
+    overflow: 'hidden',
+    ":hover": {
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.4)',
+        transform: 'scale(1.1)',
+    },
+    "::before": {
+        content: '""',
         position: 'absolute',
-        zIndex: '1300',
-        left: 0,
         top: 0,
-        display: 'flex',
-        alignItems: 'center',
-        padding: '0 10px',
-        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+        left: 0,
+        width: '100%',
+        height: '100%',
+        border: '2px solid transparent',
         borderRadius: '4px',
-        hover: 'box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4)',
+        animation: 'border-animation 1s infinite',
+        zIndex: -1,
+        pointerEvents: 'none',
+    },
+    ":hover::before": {
+        animation: 'none',
+        borderColor: '#ff0000',
+    },
+    "@keyframes border-animation": {
+        "0%": {
+            borderColor: 'transparent',
+        },
+        "50%": {
+            borderColor: '#ff0000',
+        },
+        "100%": {
+            borderColor: 'transparent',
+        },
+    },
+}}
 
-    }}
     onClick={handleGoBack}
     >
       <button
